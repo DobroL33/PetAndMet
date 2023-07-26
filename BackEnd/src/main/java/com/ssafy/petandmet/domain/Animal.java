07a212c193eb5e55f63ec0d847e3b4bc017404f9
pum005 <pum005@naver.com>
2023-07-26 17:45:42 +0900

    feat : 동물 Entity Builder 생성

--- BackEnd/src/main/java/com/ssafy/petandmet/domain/Animal.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/domain/Animal.java
@@ -1,6 +1,8 @@
 package com.ssafy.petandmet.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -8,6 +10,10 @@ import org.hibernate.annotations.GenericGenerator;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+// 입양 상태
+// 성별
+// 입소 당시 나이
+//
 @Entity
 @Table(name = "animals")
 @Getter
@@ -20,6 +26,7 @@ public class Animal {
 
     @OneToOne
     @JoinColumn(name = "center_uuid")
+    @JsonIgnore
     private Center center;
 
     @Column(name = "animal_name")
@@ -37,6 +44,23 @@ public class Animal {
 
     @Column(name = "animal_photo_url")
     private String photoUrl;
+
+    @Builder
+    public Animal(String uuid, Center center, String name, int age, String specie, String breed, String findPlace, LocalDateTime enterDate, String photoUrl) {
+        this.uuid = uuid;
+        this.center = center;
+        this.name = name;
+        this.age = age;
+        this.specie = specie;
+        this.breed = breed;
+        this.findPlace = findPlace;
+        this.enterDate = enterDate;
+        this.photoUrl = photoUrl;
+    }
+
+    public Animal() {
+
+    }
 
     //==연관관계 메서드==//
     public void setCenter(Center center) {
