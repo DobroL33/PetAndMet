ff89b9cd6b5fa74d56751e251a04e6a7ea9722ec
황준혁 <hyug0819@gmail.com>
2023-07-28 11:21:18 +0900

    feat: App에 Router 적용

--- FrontEnd/petandmet/src/App.tsx
+++ FrontEnd/petandmet/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Router from './routes/Router';
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Router></Router>
     </div>
   );
 }
