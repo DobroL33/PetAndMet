28e6d359a044812f8ba0a49624b44280b10564ab
황준혁 <hyug0819@gmail.com>
2023-07-31 16:12:49 +0900

    feat: activity Route 작성

--- FrontEnd/petandmet/src/components/user/userPage.tsx
+++ FrontEnd/petandmet/src/components/user/userPage.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import LeftMenu from './userpage/leftMenu'
 import Profile from './userpage/profile'
+import Activity from './userpage/activity'
 function UserPage() {
   return (
     <div className="flex flex-row h-screen justify-center mt-10">
@@ -10,6 +11,7 @@ function UserPage() {
       <div className="w-[50%] text-start px-10">
         <Routes>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/activity" element={<Activity />} />
         </Routes>
       </div>
     </div>
