14fe05f718c0dbddb6a07a0579f70612c6b1efd5
황준혁 <hyug0819@gmail.com>
2023-07-31 14:50:32 +0900

    feat: Router 적용

--- FrontEnd/petandmet/src/components/user/userPage.tsx
+++ FrontEnd/petandmet/src/components/user/userPage.tsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import LeftMenu from './userpage/leftMenu'
+import Profile from './userpage/profile'
 function UserPage() {
-    return (
-        <div>
-            유저 마이페이지
-        </div>
-    );
-} 
+  return (
+    <div className="flex flex-row h-screen justify-center mt-10">
+      {/* 좌측 */}
+      <LeftMenu></LeftMenu>
+      {/* 메인 */}
+      <div className="w-[50%] text-start px-10">
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
 
-export default UserPage;
+export default UserPage
