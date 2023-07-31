14716493a00c25538f2f726343017ada70d84bab
wndjs8225 <wndjs8225@naver.com>
2023-07-31 16:30:06 +0900

    fix      : App.tsx 수정

--- FrontEnd/petandmet/src/App.tsx
+++ FrontEnd/petandmet/src/App.tsx
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import MainPage from './components/mainpage';
-import NoticeList from './components/notice_list';
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      {/* <MainPage></MainPage> */}
-      <NoticeList></NoticeList>
+      <MainPage></MainPage>
     </div>
   );
 }
