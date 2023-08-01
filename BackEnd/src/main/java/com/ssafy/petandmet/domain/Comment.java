8b6dd9b92c924967fd3408b117b0a2951976bc5d
hwanhee <dup07168@naver.com>
2023-08-01 15:55:43 +0900

    refactor: 댓글파일 import 변경, JsonIgnore 추가

--- BackEnd/src/main/java/com/ssafy/petandmet/domain/Comment.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/domain/Comment.java
@@ -1,6 +1,14 @@
 package com.ssafy.petandmet.domain;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +25,13 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
+
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "user_uuid")
     private User user;
+
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "board_id")
     private Board board;
