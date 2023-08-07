f27979081fe303e997a619a1c2530b18299bdd23
황준혁 <hyug0819@gmail.com>
2023-08-07 10:13:25 +0900

    refactor: Main 경로 수정

--- FrontEnd/petandmet/src/components/Main/MainPage.tsx
+++ FrontEnd/petandmet/src/components/Main/MainPage.tsx
@@ -1,11 +1,11 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import MainLive from '../Live/LiveList';
-import AnimalList from '../Animal/AnimalList';
-import {useState} from 'react';
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import { Button } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import MainLive from 'components/Live/LiveList'
+import AnimalList from 'components/Animal/AnimalList'
+import { useState } from 'react'
 
 const btn = ['라이브', '보호동물', '봉사']
 const CustomButton = styled(Button)(({ theme }) => ({
@@ -17,48 +17,54 @@ const CustomButton = styled(Button)(({ theme }) => ({
   '&:hover': {
     backgroundColor: 'orange',
   },
-  margin : '5px',
-}));
+  margin: '5px',
+}))
 
-function MainPage(){
-    const [channel, setChannel] = useState(0);
+function MainPage() {
+  const [channel, setChannel] = useState(0)
 
-    return(
+  return (
     <>
-
-  
-    <CssBaseline />
-      <Container sx={{
-            mt : 10,
-            display: 'grid',
-            bgcolor: '#FFBC5F',
-            height: '100%',
-            borderRadius : 5,}}>
-
-      <Box sx={{
-          display: 'flex',
-          padding: '0 !important 16px'}}>
+      <CssBaseline />
+      <Container
+        sx={{
+          mt: 10,
+          display: 'grid',
+          bgcolor: '#FFBC5F',
+          height: '100%',
+          borderRadius: 5,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            padding: '0 !important 16px',
+          }}
+        >
           {btn.map((b, idx) => (
-                  <CustomButton key={idx} onClick={() => setChannel(idx)}>
-                    {b}</CustomButton>))}
-        </Box> 
+            <CustomButton key={idx} onClick={() => setChannel(idx)}>
+              {b}
+            </CustomButton>
+          ))}
+        </Box>
 
-      <Box sx={{
-            mt:1,
+        <Box
+          sx={{
+            mt: 1,
             display: 'grid',
             gridTemplateColumns: 'repeat(4, 1fr)', // 이 부분을 추가하여 카드를 자동으로 정렬합니다.
             gap: '8px', // 카드 간 간격 설정
             height: '95%',
-          }}>
-            {channel === 1 ? <AnimalList></AnimalList> : <MainLive></MainLive>}
+          }}
+        >
+          {channel === 1 ? <AnimalList></AnimalList> : <MainLive></MainLive>}
         </Box>
 
-      <CustomButton>더보기</CustomButton> 
-      {/* 더보기 눌렀을 시 라이브 리스트, 보호동물 리스트 페이지로 각각 이동 추후개발 */}
+        <CustomButton>더보기</CustomButton>
+        {/* 더보기 눌렀을 시 라이브 리스트, 보호동물 리스트 페이지로 각각 이동 추후개발 */}
       </Container>
-        </>
-    )
+    </>
+  )
 }
 
-export default MainPage;
-
+export default MainPage
--- FrontEnd/petandmet/src/components/Main/Navbar.tsx
+++ FrontEnd/petandmet/src/components/Main/Navbar.tsx
@@ -1,55 +1,59 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import logo from '../../images/new_logo.jpg';
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import Tooltip from '@mui/material/Tooltip'
+import MenuItem from '@mui/material/MenuItem'
+import AdbIcon from '@mui/icons-material/Adb'
+import logo from 'images/new_logo.jpg'
 // import { Route, Routes, Link,  useNavigate} from 'react-router-dom';
 
-const pages = ['입양', '참여 소통', '후원', '로그인', '회원가입'];
-const settings = ['입양 절차', '입양 신청', '입양 후기'];
-const notify = ['공지 사항', '봉사 신청', 'Q & A'];
-const charge = ['충전 하기', '후원 하기', '후원 후기'];
+const pages = ['입양', '참여 소통', '후원', '로그인', '회원가입']
+const settings = ['입양 절차', '입양 신청', '입양 후기']
+const notify = ['공지 사항', '봉사 신청', 'Q & A']
+const charge = ['충전 하기', '후원 하기', '후원 후기']
 
 function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  const [anchorElNotify, setAnchorElNotify] = React.useState<null | HTMLElement>(null);
-  const [anchorElCharge, setAnchorElCharge] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  )
+  const [anchorElNotify, setAnchorElNotify] =
+    React.useState<null | HTMLElement>(null)
+  const [anchorElCharge, setAnchorElCharge] =
+    React.useState<null | HTMLElement>(null)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    setAnchorElNav(event.currentTarget)
+  }
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    setAnchorElNav(null)
+  }
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElUser(event.currentTarget)
+  }
   const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    setAnchorElUser(null)
+  }
   const handleOpenNotify = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNotify(event.currentTarget);
-  };
+    setAnchorElNotify(event.currentTarget)
+  }
   const handleCloseNotify = () => {
-    setAnchorElNotify(null);
-  };
+    setAnchorElNotify(null)
+  }
   const handleOpenCharge = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElCharge(event.currentTarget);
-  };
+    setAnchorElCharge(event.currentTarget)
+  }
   const handleCloseCharge = () => {
-    setAnchorElCharge(null);
-  };
+    setAnchorElCharge(null)
+  }
 
   return (
     <AppBar position="static" sx={{ backgroundColor: '#FFA629' }}>
@@ -61,31 +65,31 @@ function Navbar() {
             component="a"
             href="/"
             sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
+              mr: 2,
+              display: { xs: 'flex', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
             }}
           >
-            <img alt='logo' src={logo} style={{width: 100}}/>              
+            <img alt="logo" src={logo} style={{ width: 100 }} />
           </Typography>
-          <Box sx = {{flexGrow : 1}}></Box>
+          <Box sx={{ flexGrow: 1 }}></Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <Box sx={{flexGrow: 1}}></Box>
-            <Box sx={{flexGrow: 1}}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box sx={{ flexGrow: 1 }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-              <MenuIcon />
-            </IconButton>
+                <MenuIcon />
+              </IconButton>
             </Box>
             <Menu
               id="menu-appbar"
@@ -106,7 +110,9 @@ function Navbar() {
               }}
             >
               {pages.map((page, index) => (
-                <MenuItem key={page} onClick={
+                <MenuItem
+                  key={page}
+                  onClick={
                     index === 0
                       ? handleOpenUserMenu
                       : index === 1
@@ -114,17 +120,18 @@ function Navbar() {
                       : index === 2
                       ? handleOpenCharge
                       : handleCloseNavMenu
-                  }>
+                  }
+                >
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          
+
           {/* 작아질 때 */}
-          
+
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-           <Box sx={{flexGrow: 1}}></Box>
+            <Box sx={{ flexGrow: 1 }}></Box>
             {pages.map((page, index) => (
               <Button
                 key={page}
@@ -166,7 +173,7 @@ function Navbar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
+              {settings.map(setting => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
@@ -188,7 +195,7 @@ function Navbar() {
               open={Boolean(anchorElNotify)}
               onClose={handleCloseNotify}
             >
-              {notify.map((item) => (
+              {notify.map(item => (
                 <MenuItem key={item} onClick={handleCloseNotify}>
                   <Typography textAlign="center">{item}</Typography>
                 </MenuItem>
@@ -210,7 +217,7 @@ function Navbar() {
               open={Boolean(anchorElCharge)}
               onClose={handleCloseCharge}
             >
-              {charge.map((item) => (
+              {charge.map(item => (
                 <MenuItem key={item} onClick={handleCloseCharge}>
                   <Typography textAlign="center">{item}</Typography>
                 </MenuItem>
@@ -220,7 +227,7 @@ function Navbar() {
         </Toolbar>
       </Container>
     </AppBar>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
