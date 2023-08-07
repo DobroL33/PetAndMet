e05215a58dc8b92410895910e2941fac39ff1fac
황준혁 <hyug0819@gmail.com>
2023-08-07 09:49:34 +0900

    refactor: User 경로 변경

--- FrontEnd/petandmet/src/components/User/Login.tsx
+++ FrontEnd/petandmet/src/components/User/Login.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import {
-  useLoginMutation,
-  LoginCredentials,
-} from '../../hooks/useLoginMutation'
+import { useLoginMutation, LoginCredentials } from 'hooks/useLoginMutation'
 function Login() {
   const [id, setId] = useState('')
   const [password, setPassword] = useState('')
