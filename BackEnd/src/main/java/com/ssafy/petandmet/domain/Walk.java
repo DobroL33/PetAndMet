635ed297f01107774567b581c88abd150d6b07fe
qkrguswns25 <qkrguswns25@naver.com>
2023-08-10 17:03:07 +0900

    feat: 산책 엔티티 수정

--- BackEnd/src/main/java/com/ssafy/petandmet/domain/Walk.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/domain/Walk.java
@@ -2,13 +2,17 @@ package com.ssafy.petandmet.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -17,6 +21,9 @@ import java.time.LocalDate;
 @Table(name = "walk")
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Walk {
 
     @Id
@@ -24,17 +31,22 @@ public class Walk {
     @Column(name = "walk_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_uuid")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "animal_uuid")
     private Animal animal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "center_uuid")
+    private Center center;
 
     private LocalDate date;
 
     private int time;
 
-    private boolean flag;
+    @Builder.Default
+    private StatusType status = StatusType.PENDING;
 }
