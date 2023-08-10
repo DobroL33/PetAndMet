6158a02831f92f3ed993dbe7d25ed362b5576e34
wndjs8225 <wndjs8225@naver.com>
2023-08-10 16:11:58 +0900

    feat     : 보호소별 후원 물품 바인딩

--- FrontEnd/petandmet/src/components/Item/ItemList.tsx
+++ FrontEnd/petandmet/src/components/Item/ItemList.tsx
@@ -15,33 +15,15 @@ import CenterDataList from 'hooks/Center/CenterMutation';
 import axios from 'axios';
 import { domain } from 'hooks/customQueryClient';
 
-const data = [
-  {
-    src: pay,
-    title: 'Don Diablo @ Tomorrowland Main Stage 2019 | Official…',
-    price: 'Don Diablo',
-    Center: '396k Center',
-  },
-  {
-    src: pay,
-    title: 'Queen - Greatest Hits',
-    price: 'Queen Official',
-    Center: '40M Center',
-  },
-  {
-    src: pay,
-    title: 'Calvin Harris, Sam Smith - Promises (Official Video)',
-    price: 'Calvin Harris',
-    Center: '130M Center',
-  },
-]
-
-interface LoadProps {
-  loading?: boolean
+interface CenterItem {
+  center_item_id: number;
+  center_uuid: string;
+  item_name: string;
+  item_target_price: number;
+  item_url: string | null;
 }
 
-function List(props: LoadProps) {
-  const { loading = false } = props
+function List() {
   const [center, setCenter] = useState('')
   const [uid, setUid] = useState('')
   const handleChange = (event: SelectChangeEvent) => {
@@ -53,7 +35,6 @@ function List(props: LoadProps) {
   useEffect(() => {
     const fetchData = async () => {
       const centersData = await CenterDataList();
-      // const centers = centersData.map((center: any) => center.);
       setCenters(centersData);
       console.log(centersData)
     };
@@ -61,7 +42,7 @@ function List(props: LoadProps) {
   }, []);
   
 
-  const [centerItem, setCenterItem] = useState<string[]>([]);
+  const [centerItem, setCenterItem] = useState<CenterItem[]>([]);
 
   const CenterItemList =async () => {
     try{
@@ -86,78 +67,75 @@ function List(props: LoadProps) {
   },[uid])
 
   if (centerItem.length !== 0) {
-  return (
-    <Container>
-      <div style={{ padding: 20 }}>
-        <Typography
-          variant="h4"
-          style={{ color: '#FFA629', fontWeight: 'bold' }}
-          >
-          후원 물품 목록
-        </Typography>
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-        <FormControl sx={{ width: '25%' }}>
-          <InputLabel id="demo-simple-select-label">보호소</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={center}
-            label="보호소"
-            onChange={handleChange}
+    return (
+      <Container>
+        <div style={{ padding: 20 }}>
+          <Typography
+            variant="h4"
+            style={{ color: '#FFA629', fontWeight: 'bold' }}
             >
-            {centers.map((cent:any) => (
-              <MenuItem value={cent.uuid} onClick={() => setUid(cent.uuid)}>{cent.name}</MenuItem>
-              ))}
-          </Select>
-        </FormControl>
-      </div>
+            후원 물품 목록
+          </Typography>
+        </div>
 
-      {/* 보호소 아이디에 해당하는 물품 map을 통해 보여주기 */}
+        <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+          <FormControl sx={{ width: '25%' }}>
+            <InputLabel id="demo-simple-select-label">보호소</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={center}
+              label="보호소"
+              onChange={handleChange}
+              >
+              {centers.map((cent:any) => (
+                <MenuItem value={cent.uuid} onClick={() => setUid(cent.uuid)}>{cent.name}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </div>
 
-      <Grid container spacing={2}>
-        {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
-          <Grid key={index} item xs={12} sm={6} md={3}>
-            <Box sx={{ width: '100%', my: 5 }}>
-              {item ? (
-                <img
-                style={{ width: '100%', height: 200, objectFit: 'cover' }}
-                alt={item.title}
-                src={item.src}
-                />
+        {/* 보호소 아이디에 해당하는 물품 map을 통해 보여주기 */}
+
+        <Grid container spacing={2}>
+          {centerItem.map((item, index) => (
+            <Grid key={index} item xs={12} sm={6} md={3}>
+              <Box sx={{ width: '100%', my: 5 }}>
+                {item ? (
+                  <img
+                  style={{ width: '100%', height: 200, objectFit: 'cover' }}
+                  alt={pay}
+                  src={pay}
+                  />
+                  ) : (
+                    <Skeleton variant="rectangular" width="100%" height={200} />
+                    )}
+                {item ? (
+                  <Box sx={{ pr: 2 }}>
+                    <Typography gutterBottom variant="body2">
+                      {item.item_name}
+                    </Typography>
+                    <Typography
+                      display="block"
+                      variant="caption"
+                      color="text.secondary"
+                      >
+                      {item.item_target_price}
+                    </Typography>
+                  </Box>
                 ) : (
-                  <Skeleton variant="rectangular" width="100%" height={200} />
-                  )}
-              {item ? (
-                <Box sx={{ pr: 2 }}>
-                  <Typography gutterBottom variant="body2">
-                    {item.title}
-                  </Typography>
-                  <Typography
-                    display="block"
-                    variant="caption"
-                    color="text.secondary"
-                    >
-                    {item.price}
-                  </Typography>
-                  <Typography variant="caption" color="text.secondary">
-                    {`${item.Center}`}
-                  </Typography>
-                </Box>
-              ) : (
-                <Box sx={{ pt: 0.5 }}>
-                  <Skeleton />
-                  <Skeleton width="60%" />
-                </Box>
-              )}
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  )
-}
+                  <Box sx={{ pt: 0.5 }}>
+                    <Skeleton />
+                    <Skeleton width="60%" />
+                  </Box>
+                )}
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    )
+  }
 else{
   return(
     <>
