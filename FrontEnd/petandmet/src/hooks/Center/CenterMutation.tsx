2ba73eb15374a48c3a797841953ad15df411bce4
wndjs8225 <wndjs8225@naver.com>
2023-08-11 10:48:42 +0900

    feat     : CenterData zustand 적용

--- FrontEnd/petandmet/src/hooks/Center/CenterMutation.tsx
+++ FrontEnd/petandmet/src/hooks/Center/CenterMutation.tsx
@@ -1,15 +1,28 @@
-import { domain } from "../customQueryClient";
-import axios from "axios";
+import { domain } from "../customQueryClient"
+import axios from "axios"
+import { create } from 'zustand'
 
 interface CenterData{
     uuid : string,
     name : string
 }
 
+interface CenterStoreState {
+  centersData: CenterData[];
+  setCentersData: (centersData: CenterData[]) => void;
+}
+
+export const useCenterStore = create<CenterStoreState>((set) => ({
+  centersData: [],
+  setCentersData: (centersData) => set({ centersData }),
+}));
+
 const CenterDataList = async () => {
     try {
       const response = await axios.get(`${domain}/center?page=0`)
       const centersData = response.data.response.boards
+
+      useCenterStore.getState().setCentersData(centersData)
       return centersData
     } catch (error) {
       console.log(error)
