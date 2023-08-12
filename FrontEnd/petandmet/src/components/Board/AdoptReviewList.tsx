fc68ea16b392ff2f347f06fa9fc95add0faa912a
이주언 <wndjs8225@naver.com>
2023-08-12 16:46:28 +0900

    feat    : 각 보호소 별 입양 후기 데이터 연동

--- FrontEnd/petandmet/src/components/Board/AdoptReviewList.tsx
+++ FrontEnd/petandmet/src/components/Board/AdoptReviewList.tsx
@@ -1,8 +1,20 @@
-import { Button } from '@mui/material'
-import Typography from '@mui/material/Typography'
+import {Button, Typography, FormControl,
+        InputLabel, MenuItem } from '@mui/material'
 import List from 'containers/components/List'
 import { useNavigate } from 'react-router-dom'
+import { useCenterStore} from 'hooks/Center/CenterMutation'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import axios from 'axios';
+import { domain } from 'hooks/customQueryClient';
+import { useState, useEffect } from 'react'
 
+interface CenterBoard {
+  id: number;
+  title: string;
+  content: string;
+  type: string;
+  board_photo_url: string | null;
+}
 
 interface Data {
   num: string
@@ -30,10 +42,54 @@ function createData(
   
   function AdoptList() {
     let navigate = useNavigate()
+    
+    const centers = useCenterStore();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+      setIsLoading(true);
+      centers.getCentersData()
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, []);
+
+    console.log(centers)
 
     const goToCreateForm = () => {
       navigate('/adoptreviewform')
     }
+    //선택된 보호소 정보 가져오는 상태, 함수
+    const [center, setCenter] = useState('')
+    const handleChange = (event: SelectChangeEvent) => {
+      setCenter(event.target.value)
+    }
+    //선택된 보호소 uuid 담는 state
+    const [uid, setUid] = useState('')
+
+    //선택된 보호소 입양후기 가져오는 상태, 함수
+    const [centerAdopt, setCenterAdopt] = useState<CenterBoard[]>([]);
+
+    const CenterAdoptList =async () => {
+      try{
+        const res = await axios.get(`${domain}/board/adopt?page=0&center_uuid=${uid}&type=adopt`)
+        console.log(res.data.response.boards.content)
+        const centerAdopt = res.data.response.boards.content
+        return centerAdopt
+      } catch(error){
+        console.log(error)
+        return [];
+      }
+    }
+    
+    useEffect(() => {
+      const fetchBoardList =async () => {
+        const centersAdopt = await CenterAdoptList()
+        setCenterAdopt(centersAdopt)
+      }
+      fetchBoardList()
+    },[uid])
+
 
     return (
     <>
@@ -44,6 +100,23 @@ function createData(
         >
           입양 후기
         </Typography>
+
+        <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+          <FormControl sx={{ width: '25%' }}>
+            <InputLabel id="demo-simple-select-label">보호소</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={center}
+              label="보호소"
+              onChange={handleChange}
+              >
+              {centers.centersData.map((cent:any) => (
+                <MenuItem value={cent.uuid} onClick={() => setUid(cent.uuid)}>{cent.name}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </div>
         <List rows={rows}></List>
       </div>
       <div style={{textAlign : 'end', width : '90%'}}>
