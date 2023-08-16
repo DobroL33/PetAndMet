014f749eb0ec057e8045e4a16db85aff99c48330
wndjs8225 <wndjs8225@naver.com>
2023-08-16 11:03:34 +0900

    refactor  : AnimalRouter 수정

--- FrontEnd/petandmet/src/routes/Animal.tsx
+++ FrontEnd/petandmet/src/routes/Animal.tsx
@@ -1,8 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import BoardFoam from 'components/Board/BoardForm'
-import BoardList from 'components/Board/BoardList'
-import BoardDetail from 'components/Board/BoardDetail'
-import BoardEdit from 'components/Board/BoardEdit'
 import AnimalDetail from 'containers/components/AnimalDetail'
 import AnimalUpdate from 'components/Animal/AnimalUpdate'
 function Animal() {
